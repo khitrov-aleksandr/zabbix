bce316fe8f89b988ba685ea80e15de0a6614dd0a
Khitrov Aleksandr <khitrov.aleksandr@gmail.com>
2020-06-02 11:30:02 +0300

    Create mysql_partition.sh

---
+++ externalscripts/mysql_partition.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+MYSQL=`which mysql`
+
+DB_NAME=zabbix
+TABLE_NAMES=(
+history
+history_log
+history_str
+history_text
+history_uint
+trends
+trends_uint
+)
+
+for X in ${TABLE_NAMES[@]}; do
+        $MYSQL -e "CALL create_partition('$X')" $DB_NAME
+        #$MYSQL -e "CALL delete_partition('$X')" $DB_NAME
+done
+
+exit 0
